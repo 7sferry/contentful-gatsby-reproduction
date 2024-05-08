ebbeced0059bdb7ac7239193323a7c2d184c807c
MR Ferry <ferferfx@gmail.com>
2024-05-08 23:16:46 +0700

    ssr with query

---
+++ src/pages/means.tsx
@@ -0,0 +1,54 @@
+/************************
+ * Made by [MR Ferry™]  *
+ * on Mei 2024          *
+ ************************/
+
+/**
+ * using ssr static routes with query
+ */
+import React from "react"
+
+const App = ({serverData}: any) => {
+    let meanings = serverData.meanings;
+
+    return (
+        <div className="app">
+            <h1>
+                this is a dictionary
+            </h1>
+            <small>
+                {serverData.word && `${serverData.word} means: ${meanings?.length > 0 ? '' : `invalid word`}`}
+            </small>
+            <ul>
+                {meanings && meanings[0]?.definitions.map((meaning: any) => {
+                    return (<li key={meaning.definition}>
+                        {`${meaning.definition}`}
+                    </li>)
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export async function getServerData(context: any) {
+    let param = context?.query?.word;
+    if (!param) {
+        return {
+            status: 200, // The HTTP status code that should be returned
+            props: {word: ''}, // Will be passed to the page component as "serverData" prop
+            headers: {},
+        };
+    }
+    let r = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + param)
+    let dict = await r.json();
+
+    let word = param;
+    let props = r.status !== 200 ? {word: word} : {word: word, meanings: dict[0].meanings};
+    return {
+        status: 200, // The HTTP status code that should be returned
+        props: props, // Will be passed to the page component as "serverData" prop
+        headers: {}, // HTTP response headers for this page
+    }
+}
+
+export default App
